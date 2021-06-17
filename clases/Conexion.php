94d83097f6ef0a9f862ce4a8d4378291d9397d55
rfloress <rudymflores77@gmail.com>
2021-06-17 00:37:58 -0600

    examen de suficiencia
    añadiendo primeros cambios para las nuevas solicitudes

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,20 +2,20 @@
 
 
 
-    $servidor= "167.114.169.207";
-    $usuario= "informat_admin";
-    $password = "HLo{Q3e{)II^";
-    $base= "informat_automatizacion";
+    $servidor= "localhost";
+    $usuario= "root";
+    $password = "";
+    $base= "copia_automatizacion";
 
-	$mysqli = new mysqli($servidor, $usuario,$password,$base);
-	$connection = mysqli_connect($servidor, $usuario,$password,$base) or die("Error " . mysqli_error($connection));
+	$mysqli = new mysqli($servidor,$usuario,$password,$base);
+	$connection = mysqli_connect($servidor,$usuario,$password,$base) or die("Error " . mysqli_error($connection));
 	
 	if($mysqli->connect_error){
 		echo "Nuestro sitio presenta fallas....";
 		die('Error en la conexion' . $mysqli->connect_error);
 		exit();	
 	}
- $connect = new PDO("mysql:host=167.114.169.207;dbname=informat_automatizacion", "informat_admin", "HLo{Q3e{)II^");
+ $connect = new PDO("mysql:host=localhost;dbname=copia_automatizacion", "root", "");
 
 if (!mysqli_set_charset($mysqli, "utf8")) {
         printf("Error cargando el conjunto de caracteres utf8: %s\n", mysqli_error($mysqli));
